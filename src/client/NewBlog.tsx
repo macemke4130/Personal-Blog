e9c49d16d880598304fa728713e1f91ef58dfda8
Lucas Mace <lucasmace4130@gmail.com>
2021-01-27 14:49:21 -0600

    More New Blog Post functionality

--- src/client/NewBlog.tsx
+++ src/client/NewBlog.tsx
@@ -5,9 +5,10 @@ import Moment from 'react-moment';
 
 const NewBlog = (props: NewBlogProps) => {
     const [allAuthors, setAllAuthros] = useState<Array<A>>([]);
+    const [theAuthor, setTheAuthor] = useState<number>(0);
+    const [theTitle, setTheTitle] = useState<string | null>("Blog Title");
+    const [theBlog, setTheBlog] = useState<string | null>("Blog Content");
     
-    let theAuthor: number | null = null;
-
     const fetchAllAuthors = async () => {
         try {
             let r = await fetch("/api/authors/");
@@ -18,8 +19,21 @@ const NewBlog = (props: NewBlogProps) => {
         }
     };
     const handleAuthorChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        theAuthor = Number(e.target.value);
-        console.log(theAuthor);
+        setTheAuthor(Number(e.target.value));
+    }
+
+    const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setTheTitle(e.target.value); 
+    }
+
+    const handleBlogChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setTheBlog(e.target.value);
+    }
+
+    const showState = () => {
+        console.log("The Author: " + theAuthor);
+        console.log("The Title: " + theTitle);
+        console.log("The Blog Text: " + theBlog);
     }
 
     useEffect(() => {
@@ -30,12 +44,15 @@ const NewBlog = (props: NewBlogProps) => {
         <>
             <h1>New Blog Post</h1>
             <div className="container">
-                <select id="authors" onChange={handleAuthorChange} value="null">
-                    <option value="null" disabled>-- Select an Author --</option>
+                <select id="authors" onChange={handleAuthorChange} >
+                    <option value="0">-- Select an Author --</option>
                     {allAuthors?.map(author => (
                         <option key={author.id} value={author.id}>{author.name}</option>
                     ))}
                 </select>
+                <label className="sr-only">Blog Title: </label><input type="text" placeholder={theTitle} onChange={handleTitleChange}></input>
+                <label className="sr-only">Chirp:</label><textarea placeholder={theBlog} onChange={handleBlogChange}></textarea>
+                <button onClick={showState}>Show State</button>
             </div>
         </>
     );
